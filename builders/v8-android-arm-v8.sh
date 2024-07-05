babf32f27666551c6d5ab5506e8a0a0126374dc8
zhuyifei <zhuyifei@bbktel.com>
2024-07-05 10:48:21 +0800

    test:

--- builders/v8-android-arm-v8.sh
+++ builders/v8-android-arm-v8.sh
@@ -37,21 +37,64 @@ git checkout $VERSION
 gclient sync
 
 
-echo "=====[ Building V8 ]====="
-python3 ./tools/dev/v8gen.py arm64.release -vv -- '
-target_os = "android"
-target_cpu = "arm64"
-v8_target_cpu = "arm64"
-is_component_build = false
-use_custom_libcxx = false
-v8_enable_i18n_support = false
-v8_use_external_startup_data = false
-v8_symbol_level = 0
-v8_static_library = true
-v8_monolithic = true
-'
+#echo "=====[ Building V8 ]====="
+#python3 ./tools/dev/v8gen.py arm64.release -vv -- '
+#target_os = "android"
+#target_cpu = "arm64"
+#v8_target_cpu = "arm64"
+#is_component_build = false
+#use_custom_libcxx = false
+#v8_enable_i18n_support = false
+#v8_use_external_startup_data = false
+#v8_symbol_level = 0
+#v8_static_library = true
+#v8_monolithic = true
+#'
 
-cd third_party && ls -a
 #ninja -C out.gn/arm64.release -t clean
 #ninja -C out.gn/arm64.release v8_monolith
 #cp ./third_party/android_ndk/sources/cxx-stl/llvm-libc++/libs/arm64-v8a/libc++_shared.so ./out.gn/arm64.release
+
+#D:\Android\Sdk\ndk\16.1.4479499\sources\cxx-stl\llvm-libc++\libs\armeabi
+
+#echo "=====[ Building ls dir ]====="
+cd third_party/android_platform && ls -a
+cd third_party/android_toolchain && ls -a
+cd third_party/android_sdk && ls -a
+cd third_party/android_sdk/ndk && ls -a
+
+#abseil-cpp
+#android_platform
+#android_sdk
+#android_toolchain
+#catapult
+#clang-format
+#colorama
+#cpu_features
+#depot_tools
+#fp16
+#fuzztest
+#glibc
+#google_benchmark_chrome
+#googletest
+#highway
+#icu
+#inspector_protocol
+#jinja2
+#jsoncpp
+#libc++
+#libc++abi
+#libunwind
+#llvm-build
+#logdog
+#markupsafe
+#ninja
+#perfetto
+#protobuf_chrome
+#re2
+#requests
+#siso
+#test262-harness
+#v8
+#wasm-api
+#zlib
